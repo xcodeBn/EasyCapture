9f9a9e8be5180775fc4cf6acf70bd5f8498bb431
XcodeBn <86073708+xcodeBn@users.noreply.github.com>
2025-09-13 11:02:57 +0300

    fixded temp bug

--- app/src/main/java/com/pisces/xcodebn/easycapture/domain/usecase/StopRecordingUseCase.kt
+++ app/src/main/java/com/pisces/xcodebn/easycapture/domain/usecase/StopRecordingUseCase.kt
@@ -2,6 +2,7 @@ package com.pisces.xcodebn.easycapture.domain.usecase
 
 import com.pisces.xcodebn.easycapture.domain.repository.RecordingRepository
 
+// usecases are so useless btw
 class StopRecordingUseCase(private val recordingRepository: RecordingRepository) {
     operator fun invoke() {
         recordingRepository.stopRecording()
